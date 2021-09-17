6a93ed47c074ccc2266b0addda221bca024ace3f
Jr.Soares <juniorsoaresti@gmail.com>
2021-09-17 11:53:36 -0400

    Configuring Chakra UI

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+import { extendTheme} from "@chakra-ui/react";
+
+export const theme  = extendTheme({
+styles: {
+  colors: {
+    white: "#FFFFFF",
+    gray: {
+      "50":  "#F5F8FA",
+      "100": "#DADADA",
+      "200": "#999999",
+      "300": "#47585B",
+    },
+    black: "#000000",
+    yellow: "#FFBA08",
+  },
+
+  global: {
+    body: {
+      bg: 'gray.50'
+    }
+  }
+}  
+})
